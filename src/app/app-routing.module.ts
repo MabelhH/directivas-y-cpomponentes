50bc46976566833c66a6a012c92ad20375ade43f
clara23 <clarahuanaco20@gmail.com>
2023-01-04 10:10:48 -0500

    actualisacion 02

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { C4Component } from './sesiones/c4/c4.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:C4Component},
+  {path:'**',pathMatch:'full',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
